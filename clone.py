4421b22d7ddd84e103b425c4ddf89479c8e68059
darien_martinez_torres <darien.martinez.torres@oracle.com>
2017-04-26 18:15:34 -0400

    Later cameras, cropping and nVidea model

--- clone.py
+++ clone.py
@@ -16,55 +16,48 @@ def getLinesFromDrivingLogs(dataPath, skipHeader=False):
             lines.append(line)
     return lines
 
-def loadImagesAndMeasurements(dataPath, skipHeader=False):
+def loadImageAndMeasurement(dataPath, imagePath, measurement, images, measurements):
+    """
+    Executes the following steps:
+      - Loads the image from `dataPath` and `imagPath`.
+      - Converts the image from BGR to RGB.
+      - Adds the image and `measurement` to `images` and `measurements`.
+      - Flips the image vertically.
+      - Inverts the sign of the `measurement`.
+      - Adds the flipped image and inverted `measurement` to `images` and `measurements`.
+    """
+    originalImage = cv2.imread(dataPath + '/' + imagePath.strip())
+    image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
+    images.append(image)
+    measurements.append(measurement)
+    # Flipping
+    images.append(cv2.flip(image,1))
+    measurements.append(measurement*-1.0)
+
+def loadImagesAndMeasurements(dataPath, skipHeader=False, correction=0.2):
     """
     Loads the images and measurements from the driving logs in the directory `dataPath`.
     If the file include headers, pass `skipHeader=True`.
+    `correction` is the value to add/substract to the measurement to use side cameras.
     Returns a pair `(images, measurements)`
     """
     lines = getLinesFromDrivingLogs(dataPath, skipHeader)
     images = []
     measurements = []
     for line in lines:
-        imagePath = line[0]
-        image = cv2.cvtColor(cv2.imread(dataPath + '/' + imagePath), cv2.COLOR_BGR2RGB)
-        images.append(image)
         measurement = float(line[3])
-        measurements.append(measurement)
-        # Flipping
-        images.append(cv2.flip(image,1))
-        measurements.append(measurement*-1.0)
+        # Center
+        loadImageAndMeasurement(dataPath, line[0], measurement, images, measurements)
+        # Left
+        loadImageAndMeasurement(dataPath, line[1], measurement + correction, images, measurements)
+        # Right
+        loadImageAndMeasurement(dataPath, line[2], measurement - correction, images, measurements)
 
     return (np.array(images), np.array(measurements))
 
-X_train, y_train = loadImagesAndMeasurements('data/data', skipHeader=True)
-
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
-
-def createPreProcessingLayers():
-    """
-    Creates a model with the initial pre-processing layers.
-    """
-    model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-    return model
-
-def leNetModel():
-    """
-    Creates a LeNet model.
-    """
-    model = createPreProcessingLayers()
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Convolution2D(6,5,5,activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
-    return model
 
 def trainAndSave(model, inputs, outputs, modelFile, epochs = 3):
     """
@@ -76,5 +69,52 @@ def trainAndSave(model, inputs, outputs, modelFile, epochs = 3):
     model.save(modelFile)
     print("Model saved at " + modelFile)
 
-model = leNetModel()
-trainAndSave(model, X_train, y_train, 'models/data.h5')
+def createPreProcessingLayers():
+    """
+    Creates a model with the initial pre-processing layers.
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
+    return model
+
+def leNetModel():
+    """
+    Creates a LeNet model.
+    """
+    model = createPreProcessingLayers()
+    model.add(Convolution2D(6,5,5,activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6,5,5,activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+def nVidiaModel():
+    """
+    Creates nVidea Autonomous Car Group model
+    """
+    model = createPreProcessingLayers()
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+
+
+print('Loading images')
+X_train, y_train = loadImagesAndMeasurements('data/data', skipHeader=True)
+# model = leNetModel()
+model = nVidiaModel()
+print('Training model')
+trainAndSave(model, X_train, y_train, 'models/nVidea_data.h5')
+print('The End')
---
+++ runOnAws.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+scp ./clone.py carnd@$1:~/CarND-Behavioral-Cloning-P3
